2920aced263a62e980a5b719c1fcf535619ed575
cwsaunders <csaunders5@liberty.edu>
2021-01-29 02:52:18 -0500

    w1

---
+++ SIE-507/Week-1/w1-assignment.py
@@ -0,0 +1,7 @@
+user_num = int(input('Enter integer:\n'))
+print("You entered:",user_num)
+print(user_num,"squared is",user_num*user_num)
+print("And",user_num,"cubed is",user_num*user_num*user_num,"!!")
+num2 = int(input('Enter another integer:\n'))
+print(user_num,"+",num2,"is",user_num+num2)
+print(user_num,"*",num2,"is",num2*user_num)

04baddc6510cfce3f475b694470c0c6062d2552b
cwsaunders <csaunders5@liberty.edu>
2021-03-29 19:41:04 -0400

    lab1

---
+++ SIE-507/Week-10/lab-1.py
@@ -0,0 +1,10 @@
+# Type all other functions here
+
+def print_menu(usr_str):
+   menu_op = ' '
+   # Complete print_menu() function
+   return menu_op, usr_str
+
+
+if __name__ == '__main__':
+    # Complete main section of code

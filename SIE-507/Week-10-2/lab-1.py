644aa1d3bfaf23579f6f110da2add5824d8fd135
cwsaunders <csaunders5@liberty.edu>
2021-04-03 17:14:31 -0400

    bug fixes and error checking

--- SIE-507/Week-10-2/lab-1.py
+++ SIE-507/Week-10-2/lab-1.py
@@ -10,9 +10,13 @@ elif highway_number < 100:
     else:
         print('I-{} is primary, going north/south.'.format(highway_number))
 elif highway_number > 100:
-    str_hw = str(highway_number)
-    str_hw_removed_digit = str_hw[1] + str_hw[2]
-    if (int(str_hw_removed_digit)%2) == 0:
-        print('I-{} is auxiliary, serving I-{}, going east/west.'.format(highway_number,str_hw_removed_digit))
-    else:
-        print('I-{} is auxiliary, serving I-{}, going north/south.'.format(highway_number,str_hw_removed_digit))
+    if highway_number < 1000:
+        str_hw = str(highway_number)
+        if str_hw[1] == '0':
+            str_hw_removed_digit = str_hw[2]
+        elif str_hw[1] != '0':
+            str_hw_removed_digit = str_hw[1] + str_hw[2]
+        if (int(str_hw_removed_digit)%2) == 0:
+            print('I-{} is auxiliary, serving I-{}, going east/west.'.format(highway_number,str_hw_removed_digit))
+        else:
+            print('I-{} is auxiliary, serving I-{}, going north/south.'.format(highway_number,str_hw_removed_digit))
